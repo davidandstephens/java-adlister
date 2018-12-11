97e7b908cdd3431947b340cd01dc2884d74315f6
davidandstephens <stephens.a.david@gmail.com>
2018-12-11 15:06:27 -0600

    finished exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -5,9 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+@WebServlet(name = "ViewProfileServlet", urlPatterns = "/login")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            if (username.equals("admin") && password.equals("password")) {
+                response.sendRedirect("profile.jsp");
+            } else {
+                response.sendRedirect("login.jsp");
+            }
+
     }
 }
