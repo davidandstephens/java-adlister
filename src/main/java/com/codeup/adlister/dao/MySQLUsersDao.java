6f31fec993a9b969222f7abf93f5e8834675cfd0
davidandstephens <stephens.a.david@gmail.com>
2018-12-14 15:29:26 -0600

    fixed a boo-boo

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -29,10 +29,10 @@ public class MySQLUsersDao implements Users {
     @Override
     public User findByUsername(String username) {
         List<User> users = new ArrayList<>();
-        String sql = "SELECT * FROM Users WHERE username LIKE ?";
+        String sql = "SELECT * FROM Users WHERE username = ?";
         try {
             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, "%" +username + "%");
+            stmt.setString(1, username);
             ResultSet rs = stmt.executeQuery();
             while(rs.next()){
                 users.add(new User(rs.getLong("id"), rs.getString("username"), rs.getString("email"), rs.getString("password")));
